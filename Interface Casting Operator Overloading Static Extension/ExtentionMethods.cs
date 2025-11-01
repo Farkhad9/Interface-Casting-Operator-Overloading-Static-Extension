40778ac6af746138d6420d2ab6c45c72f7dbd0b0
Farkhad9 <farhadai.code.edu.az>
2025-11-01 14:35:39 +0400

    first task

---
+++ Interface Casting Operator Overloading Static Extension/ExtentionMethods.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.Design;
+
+namespace Interface_Casting_Operator_Overloading_Static_Extension
+{
+    internal static class ExtentionMethods
+    {
+        public static bool IsOdd(this int a)
+        {
+            if (a % 2 != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Interface Casting Operator Overloading Static Extension/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        Console.WriteLine(a.IsOdd());
+    }
+}
